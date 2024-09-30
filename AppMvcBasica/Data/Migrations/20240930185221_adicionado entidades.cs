c46f682a9204d33a1923fa026cbdb663d0f1242a
EdinaelsonGaleno <sedinaelson@gmail.com>
2024-09-30 15:54:10 -0300

    adicionando entidades

---
+++ AppMvcBasica/Data/Migrations/20240930185221_adicionado entidades.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AppMvcBasica.Data.Migrations
+{
+    public partial class adicionadoentidades : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
